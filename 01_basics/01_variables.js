e5010123f77088641a925415d9733cf75871093e
OsamaAfzal25 <mrbaaghi25@gmail.com>
2024-09-21 12:59:00 +0000

    01_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const userId=524054
+const userName="Osama Afzal"
+let userEmail="osama054@gmail.com"
+var userAddress= "Bhera, Sargodha"
+userSports= "Cricket"
+
+console.log(userEmail)
+console.table([userId, userEmail, userAddress, userSports, userName])
